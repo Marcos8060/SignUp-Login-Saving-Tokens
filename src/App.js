d26456a3014e9975f1f75b6017dd6a07a36bc716
Marcos8060 <marcosgav80@gmail.com>
2022-06-22 00:33:03 +0300

    worked on the crud operations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
       <h3>User SignUp, Login & Saving Tokens</h3>
+      <Register />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import { AuthProvider } from "./context.js/AuthContext";
+import Profile from "./components/Profile";
+import Edit from "./components/Edit";
+import Private from "./utils/Private";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <AuthProvider>
+      <Routes>
+          <Route element={ <Private /> }>
+            <Route exact path="/profile" element={<Profile />}></Route>
+            <Route exact path="/edit/:id" element={<Edit />}></Route>
+          </Route>
+          <Route exact path="/" element={<App />}></Route>
+          <Route exact path="/home" element={<Home />}></Route>
+          <Route exact path="/register" element={<Register />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+        </Routes>
+      </AuthProvider>
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
